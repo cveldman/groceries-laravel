f6cd800c3141f440772dd8261f5ed35708af1f9a
Corné Veldman <cveldman@hotmail.com>
2022-10-11 23:22:22 +0200

    Verwijder knop

--- resources/js/Pages/Items/Index.vue
+++ resources/js/Pages/Items/Index.vue
@@ -10,8 +10,12 @@
                     <p class="text-sm font-medium text-gray-900">{{ item.product }}</p>
                     <p class="text-sm text-gray-500">-</p>
                 </div>
-                <div>
-                    <button @click="deleteItem(item.id)">Delete</button>
+                <div class="mr-3">
+                    <button type="button" @click="deleteItem(item.id)" class="inline-flex items-center rounded-full border border-transparent bg-gray-400 p-1 text-white shadow-sm hover:bg-gray-700 focus:outline-none">
+                        <svg  class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
                 </div>
             </li>
         </ul>
