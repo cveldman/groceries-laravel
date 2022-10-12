5cba987d46dbb1fc026bb2160c4bd8ad96a7337b
Corné Veldman <cveldman@hotmail.com>
2022-10-12 16:49:02 +0200

    Zoeken op producten

--- resources/js/Pages/Item/Index.vue
+++ resources/js/Pages/Item/Index.vue
@@ -1,21 +1,32 @@
 <template>
     <div>
         <div class="bg-green-500 p-4">
-            <input type="text" v-model="product" class="rounded-lg border-0 w-full"
-                   placeholder="Product toevoegen.. " v-on:keyup.enter="addItem" />
+            <h1 class="text-2xl font-semibold text-white">Boodschappen</h1>
+            <input type="text" v-model="product" placeholder="Product toevoegen"
+                   @focus="focused = true"
+                   @blur="delay(10).then(() => focused = false)"
+                   v-on:keyup.enter="addItem" class="mt-3 rounded-lg border-0 w-full" />
         </div>
 
-        <ul v-if="items.length" role="list" class="divide-y divide-gray-200">
-            <li v-for="item in items" class="flex p-4">
+        <div v-if="product || focused" class="h-40 z-10 absolute bg-gray-200 border-2 border-gray-200 left-4 right-4 mt-4">
+            <div v-if="product">
+                <div v-if="product">
+                    <p @click="addProduct(product.id)" v-for="product in filteredProducts" class="p-4 text-sm font-medium text-gray-900">
+                        {{ product.name }}
+                    </p>
+                </div>
+                <div v-else>
+                    Geen resultaten
+                </div>
+            </div>
+            <div v-else>
+                Recent
+            </div>
+        </div>
 
-                <img v-if="item.product_id" class="inline-block h-10 w-10 rounded-full mr-4"
-                     :src="products[item.product_id - 1].image" alt="Product">
-
-                <span v-else class="inline-block h-10 w-10 overflow-hidden rounded-full bg-gray-200 mr-4">
-                    <svg class="h-full w-full text-gray-500 p-2.5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
-                    </svg>
-                </span>
+        <List v-if="items.length">
+            <Item v-for="item in items">
+                <Avatar :image="item.product_id ? products[item.product_id - 1].image : null" />
 
                 <div class="flex-grow">
                     <p class="text-base font-medium text-gray-900" style="margin-top: 8px;">
@@ -30,8 +41,8 @@
                         </svg>
                     </button>
                 </div>
-            </li>
-        </ul>
+            </Item>
+        </List>
         <div v-else class="text-center pt-12">
             <svg class="mx-auto h-12 w-12 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
@@ -45,8 +56,18 @@
 <script>
 
 import { addItem, addProduct, deleteItem } from "@/Services/ItemService";
+import Input from "@/Components/Input.vue";
+import List from "@/Components/List.vue";
+import Item from "@/Components/Item.vue";
+import Avatar from "@/Components/Avatar.vue";
 
 export default {
+    components: {
+        Input,
+        List,
+        Item,
+        Avatar
+    },
     props: {
         itemss: Array,
         products: Array
@@ -54,11 +75,23 @@ export default {
     data() {
         return {
             product: null,
-            items: []
+            items: [],
+            focused: false
         };
     },
     mounted() {
         this.items = this.itemss;
+    },
+    computed: {
+        filteredProducts() {
+            return this.products.filter(product => {
+                // Filter already added products
+                return !this.items.find(item => item.product_id === product.id);
+            }).filter(product => {
+                // Filter by input
+                return product.name.toLowerCase().indexOf(this.product.toLowerCase()) !== -1;
+            });
+        }
     },
     methods: {
         async addProduct(id) {
@@ -67,6 +100,7 @@ export default {
 
                 this.items.push(product);
 
+                // TODO: Add product to recent searches
                 this.product = null;
             } catch (e) {
                 console.log(e); // TODO: Add notification when request fails
@@ -93,6 +127,9 @@ export default {
             } catch (e) {
                 console.log(e); // TODO: Add notification when request fails
             }
+        },
+        delay(time) {
+            return new Promise(resolve => setTimeout(resolve, time));
         }
     }
 }
